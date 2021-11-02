cdf852cae137849e525e263ab17952f0d4b8be72
Victor Kajiyama <Victor Kajiyama>
2021-11-02 17:46:44 -0300

    Removendo arquivos para um novo começo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 
 import ReactDOM from 'react-dom';
 import MenuButton from '../src/components/Buttons/MenuButton/Index';
+import Link from 'next/Link';
+import Button from '@material-ui/core/Button';
 
 function Home() {
 
@@ -8,10 +10,17 @@ function Home() {
         <>
             <div className="flexbox-container">
                 <div>
-                    <MenuButton text="Gerenciar pontos" backColor="#359AEC" hoverBackColor="#17466C" mainColor="#fff" hoverColor="#fff" fontSize="80px" />
+                    {/* <Link href="/gerenciar_pontos" passHref>
+                        <MenuButton text="Gerenciar pontos" backColor="#359AEC" hoverBackColor="#17466C" mainColor="#fff" hoverColor="#fff" fontSize="80px" />
+                    </Link> */}
+                    <Link href="/gerenciar_pontos" passHref>
+                        <Button variant="contained" color="secondary">About</Button>
+                    </Link>
                 </div>
                 <div>
-                    <MenuButton text="Cadastrar Cliente" fontSize="80px" hoverColor="#fff" hoverBackColor="#888888" outlined />
+                    <Link href="/cadastrar_cliente" passHref>
+                        <MenuButton text="Cadastrar Cliente" fontSize="80px" hoverColor="#fff" hoverBackColor="#888888" outlined />
+                    </Link>
                 </div>
             </div>
 
